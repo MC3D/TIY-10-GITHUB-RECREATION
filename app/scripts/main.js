4a7e5c8fd58fd041b49816fcf7b98232a9a9e6b2
MC3D <m.chapman.home@gmail.com>
2016-05-03 14:19:13 -0400

    adds repos template

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   'use strict';
 
   var username = 'MC3D';
+  var baseURL = '//api.github.com/users/';
 
   // templateId (template must have ID), container (reference to html element), model (data object)
   function renderTemplate(templateId, container, model) {
@@ -17,89 +18,46 @@ $(document).ready(function() {
     $(container).append(renderedTemplate);
   }
 
-  $.getJSON('//api.github.com/users/' + username).done(function(user){
-    $.getJSON('//api.github.com/users/'+ username + '/starred').done(function(starred){
-      $.getJSON('//api.github.com/users/'+ username + '/orgs').done(function(orgs){
-        var data = {
-          avatar: user.avatar_url,
-          username: user.login,
-          name: user.name,
-          joined: moment(user.created_at).format("MMM DD, YYYY"),
-          followers: user.followers,
-          following: user.following,
-          starred: starred.length,
-        };
+  $.getJSON(baseURL + username).done(function(user) {
+    $.getJSON(baseURL + username + '/starred').done(function(starred) {
+      $.getJSON(baseURL + username + '/orgs').done(function(orgs) {
+        $.getJSON(baseURL + username + '/repos').done(function(repos) {
+          var data = {
+            avatar: user.avatar_url,
+            username: user.login,
+            name: user.name,
+            joined: moment(user.created_at).format("MMM DD, YYYY"),
+            followers: user.followers,
+            following: user.following,
+            starred: starred.length,
+          };
 
-        renderTemplate('#template-header', '.header', data);
-        renderTemplate('#template-page', '.page', data);
+          renderTemplate('#template-header', '.header', data);
+          renderTemplate('#template-page', '.page', data);
 
-        _.each(orgs, function(org){
-          var org = {
-            name: org.login,
-            url: org.url,
-            avatar: org.avatar_url
-          }
-          renderTemplate('#template-orgs', '.orgs', org);
+          _.each(orgs, function(org) {
+            var org = {
+              name: org.login,
+              url: org.url,
+              avatar: org.avatar_url
+            }
+            renderTemplate('#template-orgs', '.orgs', org);
+          });
+
+          var repoSort = _.sortBy(repos, 'pushed_at').reverse();
+          _.each(repoSort, function(repository){
+            var repo = {
+              name: repository.name,
+              pushed: moment(repository.pushed_at).fromNow(),
+              language: repository.language,
+              stargazers: repository.stargazers_count,
+              url: repository.html_url,
+              fullname: repository.full_name
+            }
+            renderTemplate('#template-repos', '.repo-list', repo);
+          });
         });
       });
     });
   });
 });
-
-
-
-//
-//         $.getJSON("https://api.github.com/users/" + username + "/orgs").done(function(results) {
-//
-//           _.each(results, function(result) {
-//             data.org_avatar = result.avatar_url;
-//             data.org_name = result.login;
-//           });
-//
-//           // renderTemplate('#header-user', '#header-block', userData);
-//           // renderTemplate('#sidebar-user', '#sidebar', userData);
-//         });
-//     });
-//   });
-// });
-
-
-
-//
-//   $.getJSON("https://api.github.com/users/MC3D/starred").done(function(data) {
-//     userData.starred = data.length,
-//
-//     $.getJSON("https://api.github.com/users/MC3D/orgs").done(function(data) {
-//       // map produces a new array
-//       var orgData = _.map(data, function(org){
-//         return {
-//           orgAvatar: org.avatar_url,
-//           orgName: org.login
-//         };
-//       }); // closes orgData map method
-//
-//       renderTemplate('#template_header', '.header', userData);
-//       renderTemplate('#template_sidebar', '.sidebar', userData);
-//
-//       _.each(orgData, function(org){
-//         renderTemplate('#template_sidebar_orgs', '.orgs', org);
-//       }); // closes underscore for each method
-//     }); // closes JSON call for orgs
-//   }); // closes JSON call for starred
-// }); // closes JSON call for user
-//
-// $.getJSON(baseURL + '/repos').done(function(data) {
-//   var sortedData = _.sortBy(data, 'updated_at').reverse();
-//   _.each(sortedData, function(data) {
-//     var repoData = {
-//         name: data.name,
-//         updated: moment(data.updated_at).fromNow(),
-//         language: data.language,
-//         stargazersCount: data.stargazers_count,
-//         forksCount: data.forks,
-//         repoUrl: data.html_url,
-//         fullName: data.full_name
-//     };
-//     renderTemplate('#template_page_repos', '.page', repoData);
-//   });
-// });
